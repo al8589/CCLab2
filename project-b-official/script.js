88578d543ced6d8a8d47a04d6410fee5d743ceb3
al8589 <147701364+al8589@users.noreply.github.com>
2023-12-07 02:52:13 +0800

    fixing

--- project-b-official/script.js
+++ project-b-official/script.js
@@ -1,6 +1,14 @@
-<<<<<<< HEAD
 var rain = [];
 var speed;
+
+
+let mySound;
+
+
+function preload() {
+  mySound = loadSound("lib/sound/forestrain.wav");
+}
+
 
 function setup() {
   var canvas = createCanvas(windowWidth, windowHeight);
@@ -8,39 +16,23 @@ function setup() {
   for (var i = 0; i < 800; i++) {
     rain[i] = new Rain();
   }
+  mySound.play();
 }
 
+
 function draw() {
+  if(!mySound.isPlaying()){
+
+
+    mySound.loop();
+  }
   clear();
   var distance = dist(mouseX, mouseY, width / 2, height / 2);
   speed = map(distance, 0, dist(0, 0, width / 2, height / 2), 35, 0);
-  
+ 
   translate(width / 2, height / 2);
   for (var i = 0; i < rain.length; i++) {
     rain[i].update();
     rain[i].show();
   }
 }
-=======
-var rain = [];
-var speed;
-
-function setup() {
-  createCanvas(600, 600);
-  for (var i = 0; i < 800; i++) {
-    rain[i] = new Rain();
-  }
-}
-
-function draw() {
-  clear();
-  var distance = dist(mouseX, mouseY, width / 2, height / 2);
-  speed = map(distance, 0, dist(0, 0, width / 2, height / 2), 30, 0);
-   
-  translate(width / 2, height / 2);
-  for (var i = 0; i < rain.length; i++) {
-    rain[i].update();
-    rain[i].show();
-  }
-}
->>>>>>> d87814900eb67686bce6147c9d63af6861b7c15c
---
+++ project-b-official/script2.js
@@ -0,0 +1,205 @@
+console.log("Script started"); // Log to check if the script file is loaded
+
+
+let player;
+let trees = [];
+let treeCount = 11;
+
+
+let mySound; // Declare the sound variable here
+let treeImage;
+
+
+function preload() {
+  console.log("Preloading assets"); // Log to check if preload is called
+  mySound = loadSound("lib/sound/forestrain.wav", function() {
+    console.log("Sound loaded successfully"); // Log on successful load
+  }, function(error) {
+    console.error("Error loading sound:", error); // Log if there is an error
+  });
+  treeImage = loadImage('darktree.png', function() {
+    console.log("Image loaded successfully"); // Log on successful load
+  }, function(error) {
+    console.error("Error loading image:", error); // Log if there is an error
+  });
+}
+
+
+function setup() {
+  console.log("Setting up canvas"); // Log to check if setup is called
+  let canvas = createCanvas(windowWidth, windowHeight);
+  canvas.style('display', 'block');
+  canvas.parent('canvasWrapper'); // Ensure canvas is inserted into the right container
+  clear();
+  player = new Player();
+
+
+  for (let i = 0; i < treeCount; i++) {
+    let tree = new Tree(random(width), random(height), 20);
+    trees.push(tree);
+  }
+
+
+  // Play sound with user interaction
+  window.addEventListener('click', function() {
+    if (!mySound.isPlaying()) {
+      mySound.loop();
+      console.log("Sound played on click"); // Log to confirm sound plays on click
+    }
+  });
+}
+
+
+function draw() {
+  background(33, 26, 23);
+
+
+  for (let tree of trees) {
+    tree.display();
+    player.collide(tree);
+  }
+
+
+  player.update();
+  player.display();
+}
+
+
+
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    player.move(0, -2);
+  } else if (keyCode === DOWN_ARROW) {
+    player.move(0, 2);
+  } else if (keyCode === LEFT_ARROW) {
+    player.move(-2, 0);
+  } else if (keyCode === RIGHT_ARROW) {
+    player.move(2, 0);
+  }
+}
+
+
+function keyReleased() {
+  if (keyCode === UP_ARROW || keyCode === DOWN_ARROW) {
+    player.stopY();
+  }
+  if (keyCode === LEFT_ARROW || keyCode === RIGHT_ARROW) {
+    player.stopX();
+  }
+  return false;
+}
+
+
+class Player {
+  constructor() {
+    this.x = width / 2;
+    this.y = height / 2;
+    this.size = 25;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.direction = 'down';
+  }
+
+
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+
+
+    if (this.speedX > 0) this.direction = 'left';
+    else if (this.speedX < 0) this.direction = 'right';
+    else if (this.speedY > 0) this.direction = 'up';
+    else if (this.speedY < 0) this.direction = 'down';
+  }
+
+
+  display() {
+    fill(122, 31, 20);
+    noStroke();
+
+
+    ellipse(this.x, this.y, this.size, this.size);
+
+
+    this.drawBeak(); // hood reminds me of a beak/bill shape
+  }
+ 
+  drawBeak() {
+    const beakWidth = this.size * 1;
+    const beakHeight = this.size * 1.3;
+    const beakOffsetX = this.size * -0.4;
+    const beakOffsetY = 0;
+
+
+    push();
+    translate(this.x, this.y);
+    rotate(this.getBeakDirection());
+
+
+    fill(150, 35, 21);
+    beginShape();
+    vertex(beakOffsetX + beakWidth, beakOffsetY);
+    quadraticVertex(beakOffsetX + beakWidth / 2, beakOffsetY + beakHeight, beakOffsetX, beakOffsetY);
+    quadraticVertex(beakOffsetX + beakWidth / 2, beakOffsetY - beakHeight, beakOffsetX + beakWidth, beakOffsetY);
+    endShape(CLOSE);
+
+
+    pop();
+  }
+
+
+  getBeakDirection() {
+    if (this.direction === 'right') {
+      return 0;
+    } else if (this.direction === 'left') {
+      return PI;
+    } else if (this.direction === 'down') {
+      return HALF_PI;
+    }
+    return -HALF_PI;
+  }
+
+
+  move(x, y) {
+    this.speedX = x;
+    this.speedY = y;
+  }
+
+
+  stopX() {
+    this.speedX = 0;
+  }
+
+
+  stopY() {
+    this.speedY = 0;
+  }
+
+
+  collide(tree) {
+    let d = dist(this.x, this.y, tree.x, tree.y);
+    if (d < this.size / 2 + tree.radius) {
+      this.x -= this.speedX;
+      this.y -= this.speedY;
+      this.speedX = this.speedY = 0;
+    }
+  }
+}
+
+
+class Tree {
+  constructor(x, y, radius) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+  }
+
+
+  display() {
+    imageMode(CENTER);
+    image(treeImage, this.x, this.y, this.radius * 3, this.radius * 3);
+  }
+}
+
+
